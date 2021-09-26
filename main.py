ae7743195243fcc4e2e60797e3c386a8f10a3f40
yuyuko-C <shadowhuangxin@sina.com>
2021-09-26 11:02:20 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import pickle
-import pandas as pd
-from tools.upscrawler.upscrawler import UpsCrawler
-
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
-
-
-
-
-# update_userinfo()
